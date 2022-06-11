1748ba9f81153cca6c38cab6efba1d8ea50fc82b
Lambda225 <parfaitk02@gmail.com>
2022-06-11 15:51:06 +0200

    2.0.1

--- src/composant/Forum/Champenvoi.jsx
+++ src/composant/Forum/Champenvoi.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Champenvoi({schear,setschear}) {
   return (
-    <div className=' bg-white fixed bottom-0 left-0 w-full py-4'>
+    <div className=' bg-white sticky bottom-0 left-0 w-full py-4'>
         <form action="#" className='flex justify-center'>
             <div className='w-3/4 flex justify-center px-4'><textarea name="" id="" cols="2" rows="2" className='py-2 rounded-2xl w-full shadow-3xl px-4 focus:outline-0 resize-none 2xl:py-6 2xl:text-2xl ' onChange={e => {setschear(e.target.value);console.log(schear)} }></textarea></div>
             <div className='flex items-center'><button className='h-14 w-14 2xl:h-20 2xl:w-20 rounded-full bg-rose-50 flex justify-center items-center shadow-3xl'><PaperAirplaneIcon className='rotate-90 h-7 2xl:h-12' /></button></div>
--- src/pages/Admin/Adforum.jsx
+++ src/pages/Admin/Adforum.jsx
@@ -11,16 +11,15 @@ function Adforum() {
   return (
     <div className='flex bg-rose-50 h-screen'>
         <Adnavbar />
-        <div className='w-full lg:w-3/4 overflow-y-scroll'>
+        <div className='w-full lg:w-3/4 overflow-y-scroll relative'>
           <Adsearchbar /> 
-          <div className=' bg-rose-50 py-10'>
+          <div className=' bg-rose-50 pt-10'>
             <div className=" px-[6%] 2xl:px-[10%] py-12">
             {
             questions.filter(mots => mots.contenu.toLowerCase().includes(schear.toLowerCase()) ).map(question => {
                 return <Message question={question} key={question.id} admin={admin} />
               })
             }
-              <div className=' h-12'></div>
           </div>
           <Champenvoi schear={schear} setschear={setschear} />
           </div>
